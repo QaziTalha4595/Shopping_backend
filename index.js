c6a54f3859a9d3fa060a3b545e74f1687ab1c334
syed zulkharnain <96349060+Zulkharnai@users.noreply.github.com>
2023-01-12 12:24:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const api = require('./src/api');
 const app = express();
 const PORT = 3000;
 
-// parse application/x-www-form-urlencoded
+
 app.use(bodyparser.urlencoded({ extended: false }))
 
-// parse application/json
 app.use(bodyparser.json())
 app.use('/api/v1/', api);
 
